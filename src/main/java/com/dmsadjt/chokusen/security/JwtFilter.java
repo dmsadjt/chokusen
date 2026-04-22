50e617e2838af6fbd9e6b0d77443ac259315b849
Dimas <its.dee.mus@gmail.com>
2026-04-22 21:52:43 +0700

    jwtfilter init

---
+++ src/main/java/com/dmsadjt/chokusen/security/JwtFilter.java
@@ -0,0 +1,30 @@
+package com.dmsadjt.chokusen.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        FilterChain filterChain
+    ) throws ServletException, IOException {
+        String token = request.getHeader("Authorization").substring(7);
+        if (token != null && jwtUtil.isTokenValid(token)) {
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/dmsadjt/chokusen/security/JwtUtil.java
+++ src/main/java/com/dmsadjt/chokusen/security/JwtUtil.java
@@ -1,18 +1,13 @@
 package com.dmsadjt.chokusen.security;
 
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
-import javax.crypto.SecretKey;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cglib.core.Local;
-import org.springframework.stereotype.Component;
-
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
@@ -23,25 +18,36 @@ public class JwtUtil {
     @Value("${app.jwt.expiration}")
     private long expirationTime;
 
-
     public boolean isTokenValid(String token) {
-        return false;
+        try {
+            return extractClaims(token).getExpiration().after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     public String generateToken(String username) {
-        return Jwts.builder().subject(username).issuedAt(new Date()).expiration(new Date(System.currentTimeMillis() + expirationTime)).signWith(getSigningKey()).compact();
+        return Jwts.builder()
+            .subject(username)
+            .issuedAt(new Date())
+            .expiration(new Date(System.currentTimeMillis() + expirationTime))
+            .signWith(getSigningKey())
+            .compact();
     }
 
     public String extractUsername(String token) {
-        return "";
+        return extractClaims(token).getSubject();
     }
 
-    private Date getExpirationDate() {
-        return null;
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+            .verifyWith(getSigningKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
     }
 
     private SecretKey getSigningKey() {
         return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
-
 }
